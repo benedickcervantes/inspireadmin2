57b1b43a861446e970336dfc37c8721d5df4156d
ultraelectronica <fyketonel@gmail.com>
2026-01-30 12:57:56 +0800

    Update Next.js to version 16.1.6, add turbopack configuration, and include autoprefixer as a dependency

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: "/home/arnecronomica/Utilities/Projects/inspireadminv2",
+  },
 };
 
 export default nextConfig;
